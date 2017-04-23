f3ac262a01d9a7f3c2e7643de97a3956ba5199ff
santosh <santosh.ece06@gmail.com>
2017-04-23 17:16:39 +0530

    ttt

--- first.js
+++ first.js
@@ -1,11 +1,6 @@
 console.log("Hello World");
 console.log("Hello");
 console.log("Helloooo");
-
 /**/
-
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
 console.log("Hello");
 console.log("Hello");
